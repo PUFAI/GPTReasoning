7d67d0d3842c04a72b536e444b4ce2646216e5ac
michaelgathara <mikegtrm@gmail.com>
2025-03-24 23:33:01 -0500

    apparently i forgot how to import

--- reasoning/tokenization/__init__.py
+++ reasoning/tokenization/__init__.py
@@ -1,3 +1,3 @@
-from custom_tokenizer import get_custom_tokenizer
-from dataset_tokenize import tokenize_reasoning_dataset
+from .custom_tokenizer import get_custom_tokenizer
+from .dataset_tokenize import tokenize_reasoning_dataset
 __all__ = ['get_custom_tokenizer', 'tokenize_reasoning_dataset']
